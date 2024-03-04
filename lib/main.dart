1aa00954b3101a3b5e546803e5e2574915b37a4e
JaveedinCode <cavidgaffar1@gmail.com>
2024-03-04 23:11:33 +0400

    new

--- lib/main.dart
+++ lib/main.dart
@@ -22,53 +22,76 @@ class MyApp extends StatelessWidget {
             padding: const EdgeInsets.only(left:20, top:30, right: 20),
             child: Column(
               children: [
-                Container
-                (height: 50,
-                  decoration:  BoxDecoration(color: const Color.fromARGB(255, 170, 167, 167),
-                  borderRadius: BorderRadius.circular(1),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(right:170),
-                    child: ElevatedButton(style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      minimumSize: const Size.fromHeight(35.0),
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(width: 5.0, color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10.0),
+                Padding(
+                  padding: const EdgeInsets.only(bottom:12.0),
+                  child: Container
+                (
+                    height: 50,
+                    decoration:  BoxDecoration(color: const Color.fromARGB(255, 170, 167, 167),
+                    borderRadius: BorderRadius.circular(1),
+                    ),
+                    child: ListTile(
+                      title:
+                      Row(children:[
+                      Expanded(child: Padding(
+                        padding: const EdgeInsets.only(bottom:15.0, right:10),
+                        child: ElevatedButton(onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        minimumSize: const Size.fromHeight(35.0),
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(width: 1.0, color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                                            ), 
+                        child: const Text('Xidmətlər', style: TextStyle(color:Color.fromARGB(255, 42, 59, 73)),    
+                                             ),
+                                            ),
                       ),
                     ),
-                      onPressed: () {}, 
-                    child: const Row(children:[Padding(
-                      padding: EdgeInsets.only(left:30.0),
-                      child: Text("Xidmətlər", style:TextStyle(color: Color.fromARGB(255, 42, 59, 73),fontWeight: FontWeight.bold, fontSize: 20),),
-                    )
-                    ],
-                 ),
-                 ),
+                    Expanded(child: 
+                    Padding(
+                      padding: const EdgeInsets.only(bottom:15.0, left: 10),
+                      child: ElevatedButton(onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 170, 167, 167),
+                        minimumSize: const Size.fromHeight(32.0),
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(width: 1.0, color: Colors.grey),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ), child: const Text('Qurumlar üzrə', style: TextStyle(color:Color.fromARGB(255, 42, 59, 73))
+                      ),
+                      ),
+                    ),
                 ),
-               ),
+                ] 
+          ),
+          ),
+                  ),
+                ),
                   infoCard1(title:'Ailə tərkibim', subtitle: 'Ədliyyə Nazirliyi'),
                    
                   infoCard1(title:'Cərimələrim', subtitle:'Vətəndaşlara Xidmət və Sosial İnnovasiyalar üzrə Dövlət'),
-                 
+                                   
                   infoCard1(title:'COVID-19 Əks-göstəriş sertifaktım', subtitle:'İcbari Tibbı Sığorta üzrə Dövlət Agentliyi'),
-                
+                                  
                   infoCard1(title:'COVID-19 İmmuntet sertifaktım', subtitle:'İcbari Tibbı Sığorta üzrə Dövlət Agentliyi'),
-             
+                               
                   infoCard1(title:'COVID-19 Peyvənd sertifaktım', subtitle:'İcbari Tibbı Sığorta üzrə Dövlət Agentliyi'),
-                 
+                                   
                   infoCard1(title:'Daşınmaz əmlak məlumatlarım', subtitle:'Əmlak Məsələləri Dövlət Xidməti'),
-              
+                                
                   infoCard1(title:'Digər xidmət ödənişləri', subtitle:'Vətəndaşlara Xidmət və Sosial İnnovasiyalar üzrə Dövlət'),
-                 
+                                   
                   infoCard1(title:'Diplomlarım', subtitle:'Elm və Təhsil Nazirliyi'),
-               
+                                 
                   infoCard1(title:'Doğum haqqında şəhadətnamələr', subtitle:'Ədliyyə Nazirliyi'),
-              
+                                
                   infoCard1(title:'Ədliyyə və məhkəmə ödənişləri', subtitle:'Vətəndaşlara Xidmət və Sosial İnnovasiyalar üzrə Dövlət'),
-           
+                             
                   infoCard1(title:'Hərbi qeydiyyatım və xidmət barəsimdə məlumat', subtitle:'Səfərbərlik və hərbi xidmətə çağırış üzrə Dövlət xidməti'),
-             
+                               
                   infoCard1(title:'İcazələrin alınması və Monitorinq sitemi', subtitle:'Rəqəmsal inkişaf və Nəqliyyat Nazirliyi'),
               ],
             ),
